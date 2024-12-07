eb3b4a564b6035530e085c27f30555d09dad6bf9
Marjolein <mse.bente@gmail.com>
2024-12-07 18:02:44 +0100

    certificates added

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,63 +1,64 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css',
 })
 export class SkillsComponent {
   certificates = [
     {
-      title: '',
-      date: new Date(),
-      organisation: '',
+      title: 'Angular Level 1 Certification',
+      date: new Date('01-07-2024'),
+      organisation: 'Angular Training',
     },
     {
-      title: '',
-      date: new Date(),
-      organisation: '',
+      title: 'Mendix Intermediate Developer',
+      date: new Date('01-05-2024'),
+      organisation: 'Mendix',
     },
     {
-      title: '',
-      date: new Date(),
-      organisation: '',
+      title: 'Mendix Rapid Developer',
+      date: new Date('01-04-2024'),
+      organisation: 'Mendix',
     },
     {
-      title: '',
-      date: new Date(),
-      organisation: '',
+      title: 'Azure CosmosDB Developer',
+      date: new Date('01-10-2022'),
+      organisation: 'Microsoft',
     },
     {
-      title: '',
-      date: new Date(),
-      organisation: '',
+      title: 'AgileDS Foundation',
+      date: new Date('01-02-2022'),
+      organisation: 'Agile Business Consortium',
     },
     {
-      title: '',
-      date: new Date(),
-      organisation: '',
+      title: 'Power BI Data Analyst Associate',
+      date: new Date('01-05-2021'),
+      organisation: 'Microsoft',
     },
     {
-      title: '',
-      date: new Date(),
-      organisation: '',
+      title: 'Azure Data Engineer Associate',
+      date: new Date('01-03-2021'),
+      organisation: 'Microsoft',
     },
     {
-      title: '',
-      date: new Date(),
-      organisation: '',
+      title: 'Azure Data Fundamentals',
+      date: new Date('01-12-2020'),
+      organisation: 'Microsoft',
     },
     {
-      title: '',
-      date: new Date(),
-      organisation: '',
+      title: 'Programming in HTML5 with JavaScript and CSS3',
+      date: new Date('01-06-2020'),
+      organisation: 'Microsoft',
     },
     {
-      title: '',
-      date: new Date(),
-      organisation: '',
+      title: 'ITIL Foundation Level',
+      date: new Date('01-02-2020'),
+      organisation: 'PeopleCert',
     },
   ];
 
